9d6ce4c821d46e48c347370d6dd2b057706e6b69
Fuzzy Shaque <fuzzyshaque@gmail.com>
2022-10-12 11:50:37 -0700

    Lab 4

---
+++ lab4/js/lab.js
@@ -0,0 +1,28 @@
+/**
+ * Summary. (use period)
+ *
+ * Description. (use period)
+ *
+ * @link   URL
+ * @file   This files defines the MyClass class.
+ * @author Jacob Penland, Kaden Sedmak-Locke, Jorge Gonzalez.
+ * @since  x.x.x
+ */
+// Task 2: Battleship Simulation
+// Print board 1 before each turn
+// Print board 2 before each turn
+// Initialize ship objects
+//  Loop through each row
+//    Loop through each column
+//      Print S in each ship object on grid 1
+//      Accept user input
+//      If ship object exists on grid 2
+//        Print O on grid 2
+//      else
+//        Print X on grid 2
+//  If X on all ship objects
+//    Remove ship objects
+//  If all ship objects on grid 2= 0
+//    Win
+//  Else
+//    Lose
