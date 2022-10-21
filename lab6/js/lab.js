a1fd4c083fc147081d2a322f48a79836637960e1
Fuzzy Shaque <fuzzyshaque@gmail.com>
2022-10-20 20:26:00 -0700

    Lab 6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -4,9 +4,9 @@
  **/
 
 //Variables
- const myTransport= {"Bus","Skateboard","Car","Bike"}
- const myMainRide = {make:"Ferrari", model:"F355 Spider", color:"red",year:"1995",age:function(){return 2019-age}};
+myTransport= ["Bus","Skateboard","Car","Bike"];
+myMainRide = {make:"Ferrari", model:"F355 Spider", color:"red",year:"1995",age:function(){return 2019-age}};
 //Output
- document.writeln("Getting around: " + myTransport + "<br>");
+ document.writeln("The kinds of transportation I use: " + myTransport + "</br>");
  document.writeln("My Main Ride: <pre>",
     JSON.stringify(myMainRide, null, '\t'), "</pre>");
