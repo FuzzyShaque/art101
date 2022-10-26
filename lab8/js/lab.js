3df89d6226578d9631e189c8757c5f745bb796a8
Fuzzy Shaque <fuzzyshaque@gmail.com>
2022-10-26 11:10:12 -0700

    Task x lab 8

---
+++ lab8/js/lab.js
@@ -0,0 +1,22 @@
+/**
+ * Author:    Kaden Sedmak-Locke, Jacob Penland
+ * Created:   10/17/2022
+ **/
+
+//Variable declaration
+var numArray = [2, 4, 6, 8, 10];
+//Function
+function anonFunction(x) {
+    var result = x + 1;
+    return result;
+}
+//console log results
+console.log(anonFunction(2));
+console.log(anonFunction(5));
+console.log(numArray.map(anonFunction));
+console.log(numArray.map(function(x){
+    return x ** 0.5;
+}));
+var outputEl = document.getElementById("output");
+outputEl.innerHTML = "Original Array: "+ numArray + "</br> Added array: " + numArray.map(anonFunction)+ "</br> Square root array: "+ numArray.map(function(x){
+    return x ** 0.5});
