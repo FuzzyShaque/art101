2fafaa703fe22449e7bad9b48d3d2fb0ab410fb2
Fuzzy Shaque <fuzzyshaque@gmail.com>
2022-11-14 14:29:03 -0800

    css styled

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -2,20 +2,27 @@
 //  * Author:    Kaden Sedmak-Locke, Jacob Penland
 //  * Created:   10/17/2022
 var finalstring=''
+var factor1 = 3;
+var factor2 = 4;
+var factor3 = 5;
+var factor4 = 7;
 for (let i = 1; i <= 200; i++) {
   var outstring='';
-  if(i%3==0){
+  if(i%factor1==0){
     outstring+='Fizz';
   }
-  if(i%5==0){
+  if(i%factor2==0){
       outstring+='Buzz';
     }
-  if(i%7==0){
+  if(i%factor3==0){
     outstring+='Boom';
+  }
+  if(i%factor4==0){
+    outstring+='Bang!';
   }
   if (outstring.length>0){
       console.log(i+ '-'+outstring);
-      finalstring+=i+'-'+outstring+"</br>";
+      finalstring+=i+'-'+outstring+"!</br>";
   }
   else{
     console.log(i);
