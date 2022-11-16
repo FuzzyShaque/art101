e58c612006abf312fd6f9a80fc4167d4a8680ef3
Fuzzy Shaque <fuzzyshaque@gmail.com>
2022-11-16 12:25:44 -0800

    Lists

---
+++ lab14/js/lab.js
@@ -0,0 +1,42 @@
+// /**
+//  * Author:    Kaden Sedmak-Locke, Jacob Penland
+//  * Created:   10/17/2022
+
+
+
+//Function to determine and output the factor-phrase relation
+function fizzBuzz(max, numInput, txtInput) {
+  //Empty Final String Declaration
+  var finalstring=''
+  //for loop that iterates within the inputed range
+  for (var num = 1; num <= max; num++) {
+    var outstring='';
+    //for loop that iterates through each position of the matching arrays
+    for (var input in txtInput){
+      if (num % numInput[input] == 0) {
+        outstring += txtInput[input];
+      }
+    }
+    //conditional for checking if there is a factor in the first place and appending the combination
+    if (outstring.length>0){
+        console.log(num+ '-'+outstring);
+        finalstring+=num+'-'+outstring+"!</br>";
+    }
+    //otherwise just output the raw number
+    else{
+      console.log(num);
+      finalstring+=num+"</br>";
+    }
+}
+//appending the result of the function iteration
+$('#output').html(finalstring);
+}
+//button event listener, that initializes the 2 arrays of field inputs
+$('#button').click(function() {
+
+  var numArr=[$('#num0').val(),$('#num1').val(),$('#num2').val(),$('#num3').val()];
+
+  var inputArr=[$('#input1').val(),$('#input2').val(),$('#input3').val(),$('#input4').val()];
+
+  fizzBuzz($('#input0').val(),numArr, inputArr);
+});
